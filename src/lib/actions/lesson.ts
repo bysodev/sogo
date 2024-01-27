7e3575ac776b4ff925d5e4d25e65c4018968021f
Hoshisz <66334480+Anthonymgd@users.noreply.github.com>
2024-01-27 00:21:58 -0500

    Actualizar lesson.ts

--- src/lib/actions/lesson.ts
+++ src/lib/actions/lesson.ts
@@ -18,7 +18,7 @@ export async function PredictSign(
   });
 
   try {
-    const res = await fetch(`http://127.0.0.1:8000/user/lesson/predict`, {
+    const res = await fetch(`{process.env.NEXT_PUBLIC_API_BACKEND}/user/lesson/predict`, {
       method: 'POST',
       headers: myHeaders,
       body: raw,
