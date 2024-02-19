338578d5b8c1c053abaa4cde04c068e1d188fa52
Hoshisz <66334480+Anthonymgd@users.noreply.github.com>
2024-02-19 09:14:14 -0500

    async headers mix http with https

--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "upgrade-insecure-requests",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
