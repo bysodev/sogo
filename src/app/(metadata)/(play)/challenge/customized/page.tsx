1aa5bfe890cf5ea84ab7e23c75737712deaaddb9
Hoshisz <66334480+Anthonymgd@users.noreply.github.com>
2024-02-20 15:34:56 -0500

    fix: update is valid predict

--- src/app/(metadata)/(play)/challenge/customized/page.tsx
+++ src/app/(metadata)/(play)/challenge/customized/page.tsx
@@ -415,14 +415,15 @@ export default function ChallengesPage() {
     };
 
     const isValidResult = (char: string, result: string) => {
-      return char === result || 
-             (char === '0' && result === 'O') || 
-             (char === 'O' && result === '0') ||
-             (char === 'T' && result === '9') ||
-             (char === '9' && result === 'T') ||
-             (char === 'U' && result === 'R') ||
-             (char === 'R' && result === 'U');
-    }
+    return char === result || 
+           (char === '0' && result === 'O') || 
+           (char === 'O' && result === '0') ||
+           (char === 'T' && result === '9') ||
+           (char === '9' && result === 'T') ||
+           (char === 'U' && result === 'R') ||
+           (char === 'R' && result === 'U') ||
+           (char === 'F' && result === '9') ||
+  }
       
     const handleVerification = async () => {
         setSubmit(false);
