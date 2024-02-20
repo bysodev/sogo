3808810f183d2c48b1814fbd5bbdee1163a42f48
Hoshisz <66334480+Anthonymgd@users.noreply.github.com>
2024-02-20 10:22:48 -0500

    build: new endpoint to verify lesson image

---
+++ src/app/api/auth/predict/route.tsx
@@ -0,0 +1,43 @@
+const url = process.env.NEXT_PUBLIC_API_BACKEND
+import { getServerSession } from "next-auth/next";
+import { config } from "./../[...nextauth]/route";
+export async function POST(request: Request) {
+  const { username, password, email, category, image, char, token } = await request.json()
+  
+  if (!username || !password || !email || !category || !image || !char || !token) {
+    return new Response('No se logro la petición, faltan campos', {
+      status: 401,
+    })
+  }
+  const session = await getServerSession(config)
+  const myHeaders = new Headers({
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  })
+  myHeaders.append('Authorization', `Bearer ${session?.user.accessToken}`)
+  try {
+    const response = await fetch(`${url}/user/lesson/predict`, {
+      method: 'POST',
+      body: JSON.stringify({
+        username,
+        password,
+        email,
+        image: username,
+      }),
+      credentials: 'include',
+      headers: myHeaders,
+      redirect: 'follow'
+    })
+    const data = await response.json();
+    if (response.status === 201) {
+      return Response.json(data, {
+        status: response.status
+      })
+    }
+    return new Response(data.detail, { status: response.status })
+  } catch (error) {
+    return new Response('Problemas con el servidor', {
+      status: 501,
+    })
+  }
+}
