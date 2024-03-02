467033b8a508e4a5afd277f313432c9be9b020d5
Anthonymgd <amgoyes@espe.edu.ec>
2024-03-02 03:46:12 -0500

    style: fixed images letra_X

--- src/lib/actions/globales.ts
+++ src/lib/actions/globales.ts
@@ -1,7 +1,10 @@
 export const isValidResult = (char: string, result: string) => {
-    return char === result || 
-           (char === 'U' && result === 'R') ||
-           (char === 'R' && result === 'U') ||
-           (char === 'N' && result === 'M') ||
-           (char === 'M' && result === 'N');
-}
+  return (
+    char === result ||
+    (char === 'U' && result === 'R') ||
+    (char === 'R' && result === 'U') ||
+    (char === 'N' && result === 'M') ||
+    (char === 'M' && result === 'N') ||
+    (char === 'P' && result === 'Q')
+  );
+};
