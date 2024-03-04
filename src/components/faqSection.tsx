478140904a23c7ce7ad43b954be6e3731de976f4
bysodev <thealfred0210@outlook.com>
2024-03-04 07:44:12 -0500

    fix: mejoras en el sobre nosotros y preguntas frecuentes

--- src/components/faqSection.tsx
+++ src/components/faqSection.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { Accordion, AccordionDetails, AccordionSummary } from "@mui/material"
+import Link from "next/link"
 import { MdExpandMore } from "react-icons/md"
 
 export default function FaqSection() {
@@ -14,6 +15,48 @@ export default function FaqSection() {
             <h1 className="h2 mb-4">Preguntas frecuentes</h1>
           </div>
           <div className='grid gap-4 text-balance'>
+            <Accordion className='shadow-lg btn block p-1 dark:text-white dark:bg-gray-800'>
+              <AccordionSummary
+                expandIcon={<MdExpandMore className='dark:text-white' size={25} />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <h3 className='text-base md:text-lg xl:text-xl font-bold'>¡Empieza a utilizar Sogo Sign!</h3>
+              </AccordionSummary>
+              <AccordionDetails>
+                <p>
+                  Dirigite a <Link href={'/auth/register'} className="text-purple-600 dark:text-purple-400 text-lg">registrarme</Link>  y crea una cuenta, necesitas un correo electrónico personal al cual te pueda hacer llegar un correo de verificación y finalmente una redirección al aplicativo para finalizar el proceso, en cuestión de segundos todo estará listo para que puedas empezar.
+                </p>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion className='shadow-lg btn block p-1 dark:text-white dark:bg-gray-800'>
+              <AccordionSummary
+                expandIcon={<MdExpandMore className='dark:text-white' size={25} />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <h3 className='text-base md:text-lg xl:text-xl font-bold'>¿Cómo crear mi propio reto?</h3>
+              </AccordionSummary>
+              <AccordionDetails>
+                <p>
+                  Primero hay que terminar los retos de una categoría y nivel de dificultad en específico, al querer comenzar cuando la barra de progreso esté llena lo llevará a crear su nuevo reto. Pero si tienes creatividad puedes crear retos ilimitados con un truquito
+                </p>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion className='shadow-lg btn block p-1 dark:text-white dark:bg-gray-800'>
+              <AccordionSummary
+                expandIcon={<MdExpandMore className='dark:text-white' size={25} />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <h3 className='text-base md:text-lg xl:text-xl font-bold'>¿Por qué debo activar la cámara en el módulo de aprendizaje?</h3>
+              </AccordionSummary>
+              <AccordionDetails>
+                <p>
+                  Se explica que la funcionalidad de la cámara solo es para practicar y no se la usa para validar nada, sin embargo, hemos detectado buen uso de este componente al practicar de forma grupal o de forma individual con mejores resultados en memorizar las señas de los temarios.
+                </p>
+              </AccordionDetails>
+            </Accordion>
             <Accordion className='shadow-lg btn block p-1 dark:text-white dark:bg-gray-800'>
               <AccordionSummary
                 expandIcon={<MdExpandMore className='dark:text-white' size={25} />}
@@ -60,6 +103,23 @@ export default function FaqSection() {
                   B) Contar con una conexión a internet rápida y estable. <br />
                   C) Procurar que en el encuadre de la foto solo sea visible la mano con la seña respectiva.
                 </p>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion className='shadow-lg btn block p-1 dark:text-white dark:bg-gray-800'>
+              <AccordionSummary
+                expandIcon={<MdExpandMore className='dark:text-white' size={25} />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <h3 className='text-base md:text-lg xl:text-xl font-bold'>¡Aun tengo algunas dudas, como los contacto!</h3>
+              </AccordionSummary>
+              <AccordionDetails>
+                <p>
+                  Nos encantaría poder ayudarte, puedes contactarnos desde <a className="text-purple-600 dark:text-purple-400 text-lg" href="#contact">
+                  aquí
+                  </a> o directamente a nuestro correo electrónico: educaciondigital@espe.edu.ec.
+                </p>
+                
               </AccordionDetails>
             </Accordion>
           </div>
